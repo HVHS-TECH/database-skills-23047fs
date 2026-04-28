238fa9afdf393ba53c5b0c4ed69bdf40f8e4dc1e
23047fs <23047fs@hvhs.school.nz>
2026-04-28 14:28:55 +1200

    reading

--- script.js
+++ script.js
@@ -16,40 +16,43 @@ const HTML_OUTPUT = document.getElementById("databaseOutput");
 // This uses the set() operation to write the key:value pair "message":"Hello World"
 // The ref('/') part tells the operation to write to the base level of the database "/"
 // This means it replaces the whole database with message:Hello World
-// Adds Ben and his data
 /**************************************************************/
-function userData(){
-  console.log("Running helloWorld()")
-  firebase.database().ref('/').set(
-    {
-      users: {
-        '01': '',
-        '02': '',
-        '03': ''
-      }
-    }
-  )
+function simpleWrite(){
+  console.log("Running simpleWrite()");
+  //Creating data from '/' (roots) then in 'users: 01...' adding data
   firebase.database().ref('/users/01').set(
     {
       name: 'ben',
       age: 102,
       hair: 'yellow'
     }
-  )
+  );
   firebase.database().ref('/users/02').set(
     {
       name: 'jack',
       age: 4,
       hair: 'null'
     }
-  )
+  );
   firebase.database().ref('/users/03').set(
     {
       name: 'jo',
       age: 52,
       hair: 'black'
     }
-  )
+  );
+}
+
+function simpleRead() {
+  console.log("Reading message");
+  //Reading from '' then doing function displayRead
+  firebase.database().ref('/users/02/name').once('value', displayRead);
+  console.log("Leaving simpleRead");
+}
+
+function displayRead(snapshot) {
+  console.log("Running displayRead(), the message is: " + snapshot.val());
+  HTML_OUTPUT.innerHTML = snapshot.val();
 }
 
 /**************************************************************/
